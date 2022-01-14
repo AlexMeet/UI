b9d7c7ce35aa0389ff518e3fa5bebc0e99e5c3e4
Aleksey <81695388+AlexMeet@users.noreply.github.com>
2022-01-14 16:28:12 +0500

    Add files via upload

---
+++ test_one/app/src/main/java/com/example/test_one/Magnetomer.java
@@ -0,0 +1,14 @@
+package com.example.test_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Magnetomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magnetomer);
+    }
+}
